5f85d7e945eb9d89356e3be22b23aba2abc74231
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-02-07 19:34:33 +0500

    Improvements

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 minetest.register_chatcommand("ec", {
     description = "Open enderchest",
     func = function()
-core.show_formspec('ec','size[8,9]list[current_player;enderchest;0,0;8,4]list[current_player;main;0,5;8,4]listring[current_player;enderchest]listring[current_player;main]')
+core.show_formspec('ec','size[8,9]list[current_player;enderchest;0,0;8,4]list[current_player;main;0,5;8,4]listring[]list[detached:trash;main;7,4;1,1]image[6,4;1,1;creative_trash_icon.png]')
     end,
 })
